648168b8622fccafa83446a14a97116375295554
اسمك هنا < >
2026-01-25 19:14:04 +0300

    تعديل الستينق

--- communication/settings.py
+++ communication/settings.py
@@ -15,6 +15,13 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 # =========================
+# ☁️ Cloudinary ENV (Direct)
+# =========================
+os.environ["CLOUDINARY_CLOUD_NAME"] = "dyg4401o9"
+os.environ["CLOUDINARY_API_KEY"] = "489947491336852"
+os.environ["CLOUDINARY_API_SECRET"] = "ybw_lynZTuhxcRbbQ1NfIVZT9r8UR_API_SECRET"
+
+# =========================
 # 📦 Applications
 # =========================
 INSTALLED_APPS = [
